111f7d20ff8fce083569a38ddf050fa93a0c2d3c
Oleg Kochiev <developer-artisan@mail.ru>
2023-07-13 19:12:38 +0300

    added loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Loader from '@/components/Loader';
+import '@/styles/globals.css';
+import type {AppProps} from 'next/app';
+import {Router} from 'next/router';
+import {useState} from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', (url) => {
+    setIsLoading(true);
+  });
+  Router.events.on('routeChangeComplete', (url) => {
+    setIsLoading(false);
+  });
+  return isLoading ? <Loader /> : <Component {...pageProps} />;
 }
